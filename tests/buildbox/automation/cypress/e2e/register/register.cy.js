3b53c935e03ff223b6200830797d9d815e1df7c7
thejohnjohn <jp7480566@gmail.com>
2024-10-22 22:19:44 -0300

    tests: gatting started with automation tests

---
+++ tests/buildbox/automation/cypress/e2e/register/register.cy.js
@@ -0,0 +1,13 @@
+beforeEach(() => {
+    cy.visit('https://qastage.buildbox.one/18/cadastro');
+})
+
+describe(`Buildbox registration`, () => {
+    it('Checking the personal data step', () => {
+
+    });
+
+    it('Checking the address data step', () => {
+
+    });
+});
